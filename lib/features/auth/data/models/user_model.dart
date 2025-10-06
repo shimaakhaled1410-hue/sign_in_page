c124936dc5bc924b129ddf950092a57b8776b15e
shimaakhaled1410-hue <shimaakhaled1410@gmail.com>
2025-10-06 06:53:32 +0300

    edit user model

--- lib/features/auth/data/models/user_model.dart
+++ lib/features/auth/data/models/user_model.dart
@@ -1,9 +1,30 @@
 import 'package:sign_in_page/features/auth/domain/entities/user_entity.dart';
 
-class UserModel extends UserEntity {
-  UserModel({required super.id, required super.email});
+class UserModel extends UserEntity{
+  final int id;
+  final String username;
+  final String email;
+  final String firstName;
+  final String lastName;
+  final String accessToken;
+
+  UserModel({
+    required this.id,
+    required this.username,
+    required this.email,
+    required this.firstName,
+    required this.lastName,
+    required this.accessToken,
+  }) : super(userId: id, userEmail: email);
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
-    return UserModel(id: json['id'] ?? '', email: json['email'] ?? '');
+    return UserModel(
+      id: json['id'],
+      username: json['username'],
+      email: json['email'],
+      firstName: json['firstName'],
+      lastName: json['lastName'],
+      accessToken: json['accessToken'],
+    );
   }
 }
